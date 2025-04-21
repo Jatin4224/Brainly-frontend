558f654997751b020e24cb3210b8c3dc3a862975
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-04-21 09:50:26 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  return <div className="bg-black">App</div>;
 };
 
 export default App;
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,1 +1,12 @@
-npm install tailwindcss @tailwindcss/vite
+interface Buttonprops {
+  varient: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  Text: String;
+  startIcon?: any;
+  endIcon?: any;
+  onclick: () => void;
+}
+
+export const Button = (props: ButtonProps) => {
+  return <button></button>;
+};
